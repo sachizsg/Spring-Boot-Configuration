a2748820d1f3c900f07188c5d72dd979782d83e2
sachizsg <wrsachithra@gmail.com>
2020-05-05 14:02:19 +0530

    Fetch value from propertyFile using @Value annotation

--- spring-boot-config/src/main/java/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/springbootconfig/GreetingController.java
@@ -1,5 +1,8 @@
 package springbootconfig;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +14,32 @@ public class GreetingController {
 	 * Inject the value of the member variable| Fetch value from propertyFile 
 	 * Config microservice using @value 
 	 */
-	@Value("${my.greeting}")
+	@Value("${my.greeting: default value}")
 	private String greetingMessage;
 	
+	@Value("some static message")
+	private String staticMessage;
 	
+	@Value("${my.list.values}")
+	private List<String> listValues;
+	
+	@Value("#{${dbValues}}")
+	private Map<String, String> dbValues;
+	
+	/**
+	 * Default constructor
+	 */
+	public GreetingController() {
+		
+	}
+	
+	/**
+	 * Greeting 
+	 * @return greetingMessage
+	 */
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greetingMessage;
+		return greetingMessage + staticMessage + listValues + dbValues;
 	}
 	
 
