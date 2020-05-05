55592f557cfc742986e1ccf0bad5ab6563fd94da
sachizsg <wrsachithra@gmail.com>
2020-05-05 14:32:14 +0530

    Add ConfigurationProperty for DbSettings

--- spring-boot-config/src/main/java/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/springbootconfig/GreetingController.java
@@ -1,8 +1,8 @@
 package springbootconfig;
 
 import java.util.List;
-import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +23,8 @@ public class GreetingController {
 	@Value("${my.list.values}")
 	private List<String> listValues;
 	
-	@Value("#{${dbValues}}")
-	private Map<String, String> dbValues;
-	
+	@Autowired
+	private DbSettings dbSettings;
 	/**
 	 * Default constructor
 	 */
@@ -39,7 +38,7 @@ public class GreetingController {
 	 */
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greetingMessage + staticMessage + listValues + dbValues;
+		return dbSettings.getConnection() + dbSettings.getHost() ;
 	}
 	
 
